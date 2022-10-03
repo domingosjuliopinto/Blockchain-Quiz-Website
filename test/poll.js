9098782d50fa5e36d874ea80a5dd7b3b9c97e096
Domingos Julio Pinto <domingosjuliopinto@gmail.com>
2022-10-04 00:20:18 +0530

    Fourth commit

--- test/poll.js
+++ test/poll.js
@@ -1,29 +1,86 @@
 var Poll = artifacts.require("./Poll.sol");
 
 contract("Poll", function(accounts) {
-    var PollInstance;
+  var PollInstance;
 
-    it("initializes with two candidates", function() {
-        return Poll.deployed().then(function(instance) {
-            return instance.candidatesCount();
-        }).then(function(count) {
-            assert.equal(count, 2);
-        });
+  it("initializes with two candidates", function() {
+    return Poll.deployed().then(function(instance) {
+      return instance.candidatesCount();
+    }).then(function(count) {
+      assert.equal(count, 2);
     });
+  });
 
-    it("it initializes the candidates with the correct values", function() {
-        return Poll.deployed().then(function(instance) {
-            PollInstance = instance;
-            return PollInstance.candidates(1);
-        }).then(function(candidate) {
-            assert.equal(candidate[0], 1, "contains the correct id");
-            assert.equal(candidate[1], "Candidate 1", "contains the correct name");
-            assert.equal(candidate[2], 0, "contains the correct votes count");
-            return PollInstance.candidates(2);
-        }).then(function(candidate) {
-            assert.equal(candidate[0], 2, "contains the correct id");
-            assert.equal(candidate[1], "Candidate 2", "contains the correct name");
-            assert.equal(candidate[2], 0, "contains the correct votes count");
-        });
+  it("it initializes the candidates with the correct values", function() {
+    return Poll.deployed().then(function(instance) {
+      PollInstance = instance;
+      return PollInstance.candidates(1);
+    }).then(function(candidate) {
+      assert.equal(candidate[0], 1, "contains the correct id");
+      assert.equal(candidate[1], "Candidate 1", "contains the correct name");
+      assert.equal(candidate[2], 0, "contains the correct votes count");
+      return PollInstance.candidates(2);
+    }).then(function(candidate) {
+      assert.equal(candidate[0], 2, "contains the correct id");
+      assert.equal(candidate[1], "Candidate 2", "contains the correct name");
+      assert.equal(candidate[2], 0, "contains the correct votes count");
     });
+  });
+
+  it("allows a voter to cast a vote", function() {
+    return Poll.deployed().then(function(instance) {
+      PollInstance = instance;
+      candidateId = 1;
+      return PollInstance.vote(candidateId, { from: accounts[0] });
+    }).then(function(receipt) {
+      return PollInstance.voters(accounts[0]);
+    }).then(function(voted) {
+      assert(voted, "the voter was marked as voted");
+      return PollInstance.candidates(candidateId);
+    }).then(function(candidate) {
+      var voteCount = candidate[2];
+      assert.equal(voteCount, 1, "increments the candidate's vote count");
+    })
+  });
+
+  it("throws an exception for invalid candiates", function() {
+    return Poll.deployed().then(function(instance) {
+      PollInstance = instance;
+      return PollInstance.vote(99, { from: accounts[1] })
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+      return PollInstance.candidates(1);
+    }).then(function(candidate1) {
+      var voteCount = candidate1[2];
+      assert.equal(voteCount, 1, "candidate 1 did not receive any votes");
+      return PollInstance.candidates(2);
+    }).then(function(candidate2) {
+      var voteCount = candidate2[2];
+      assert.equal(voteCount, 0, "candidate 2 did not receive any votes");
+    });
+  });
+
+  it("throws an exception for double voting", function() {
+    return Poll.deployed().then(function(instance) {
+      PollInstance = instance;
+      candidateId = 2;
+      PollInstance.vote(candidateId, { from: accounts[1] });
+      return PollInstance.candidates(candidateId);
+    }).then(function(candidate) {
+      var voteCount = candidate[2];
+      assert.equal(voteCount, 1, "accepts first vote");
+      // Try to vote again
+      return PollInstance.vote(candidateId, { from: accounts[1] });
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message, "error message must contain revert");
+      return PollInstance.candidates(1);
+    }).then(function(candidate1) {
+      var voteCount = candidate1[2];
+      assert.equal(voteCount, 1, "candidate 1 did not receive any votes");
+      return PollInstance.candidates(2);
+    }).then(function(candidate2) {
+      var voteCount = candidate2[2];
+      assert.equal(voteCount, 0, "candidate 2 did not receive any votes");
+    });
+  });
 });
